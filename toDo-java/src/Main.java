9c8fdc48124ae1a37cb1025c7ebf64338820b2be
Kirill <kirillsavorin@gmail.com>
2025-08-09 20:45:57 +0200

    ref: new function

--- toDo-java/src/Main.java
+++ toDo-java/src/Main.java
@@ -4,15 +4,18 @@ import java.awt.*;
 public class Main {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
-      JFrame frame = new JFrame("My toDo");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(800, 600);
-      frame.setLayout(new BorderLayout());
-
-      JLabel label = new JLabel("Welcome to my ToDo", SwingConstants.CENTER);
-      frame.add(label, BorderLayout.CENTER);
-
-      frame.setVisible(true);
+      createAndShowGUI();
     });
     }
+
+  private static void createAndShowGUI() {
+    JFrame frame = new JFrame("My toDo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(800, 600);
+    frame.setLayout(new BorderLayout());
+    JLabel panel = new JLabel("Welcome to my ToDo", SwingConstants.CENTER);
+    frame.add(panel, BorderLayout.CENTER);
+
+    frame.setVisible(true);
   }
+}
